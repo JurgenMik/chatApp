1a16ff6e25d7f07dbf04a8a0705709e4ea78983d
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-07-27 17:35:19 +0300

    Implemented cond. rendering via switch & passed ProfileName as props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import Chat from './components/Chat'
+import {useState} from "react"
 import Avatar from '@mui/material/Avatar'
 import Badge from '@mui/material/Badge'
 
 function App() {
+
+    const [name, setName] = useState('')
+
+    const TargetName = (e) => {
+        setName(e.target.alt);
+    }
+
   return (
       <div className="w-full h-screen grid grid-cols-3 overflow-hidden">
         <div className="bg-slate-700 h-full w-2/3 sm:w-1/3">
@@ -13,7 +21,7 @@ function App() {
           <div className="h-full w-full grid grid-cols-1 pt-12">
             <div className="flex justify-center">
                 <Badge badgeContent="" overlap="circular" color="success">
-                    <Avatar alt="Madis" src={require("./assets/profPic1.jpg")} sx={{
+                    <Avatar alt="Madis" onClick={TargetName} src={require("./assets/profPic1.jpg")} sx={{
                         height: '125px',
                         width: '125px',
                     }}/>
@@ -21,7 +29,7 @@ function App() {
             </div>
             <div className="flex justify-center -mt-8">
                 <Badge badgeContent="" overlap="circular" color="success">
-                    <Avatar alt="Kalle" src={require("./assets/profPic3.jpg")} sx={{
+                    <Avatar alt="Kalle" onClick={TargetName} src={require("./assets/profPic3.jpg")} sx={{
                         height: '125px',
                         width: '125px'
                     }}/>
@@ -29,7 +37,7 @@ function App() {
             </div>
             <div className="flex justify-center -mt-8">
                 <Badge badgeContent="" overlap="circular" color="success">
-                    <Avatar alt="Laura" src={require("./assets/profPic2.jpg")} sx={{
+                    <Avatar alt="Laura" onClick={TargetName} src={require("./assets/profPic2.jpg")} sx={{
                         height: '125px',
                         width: '125px'
                     }}/>
@@ -37,7 +45,7 @@ function App() {
             </div>
             <div className="flex justify-center -mt-8">
               <Badge badgeContent="" overlap="circular" color="warning">
-                  <Avatar alt="Laura" src={require("./assets/profPic4.jpg")} sx={{
+                  <Avatar alt="Kadri" src={require("./assets/profPic4.jpg")} sx={{
                       height: '125px',
                       width: '125px'
                   }}/>
@@ -46,7 +54,18 @@ function App() {
           </div>
         </div>
         <div className="col-span-2">
-            <h1 className="font-bold pt-4">Click on a profile to begin chatting..</h1>
+            {(() => {
+                switch(name) {
+                    case 'Madis':
+                        return <Chat profileName='Madis'/>;
+                    case 'Kalle':
+                        return <Chat profileName='Kalle'/>;
+                    case 'Laura':
+                        return <Chat profileName='Laura'/>;
+                    default:
+                        return <h1 className="font-bold pt-4">Click on a profile to begin chatting..</h1>
+                }
+             })()}
         </div>
       </div>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,2 +1,12 @@
+import ChatMessages from './ChatMessages'
 
+function Chat({profileName}){
+    return(
+        <div>
+            <h1>{profileName}</h1>
+        </div>
+    );
+}
+
+export default Chat;
 
